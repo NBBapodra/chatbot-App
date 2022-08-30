9d0e419aac165867b2804a305083b311e0b66edf
NBBapodra <niteen.urteqi@gmail.com>
2022-08-30 15:45:56 +0530

    Initial commit

---
+++ src/test/java/com/example/chatbot/App/ChatbotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbot.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
